f285060779046193dc5aef00c905f3e84ee63ecb
nuhgnc <Nuh16358@gmail.com>
2022-05-17 22:01:29 +0300

    2.odev

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Konsol üzerinden paketlerimizi yüklüyoruz '> npm install apollo-server graphql ' daha sonra projeye dahil ediyoruz
 const { ApolloServer, gql } = require('apollo-server');
 
+const  { nanoid }  = require ('nanoid')
+
 // Şimdi data.json dosyasını projemize dahil edelim
 const {events, locations, users, participants} = require('./data.json');
 
@@ -40,6 +42,21 @@ const typeDefs = gql`
     event_id: ID!
     username: [User]
   },
+
+  type DeleteAllUser {
+    count: Int,
+    deleted_users: [User]!
+  }
+
+ # Veri Girişlerini oluşturalım
+  input InputCreateUser {
+    username: String!,
+    email: String!
+  },
+  input InputUpdateUser {
+    username: String,
+    email: String
+  }
  
  # Queryleri oluşturlım
   type Query {
@@ -58,6 +75,16 @@ const typeDefs = gql`
     locations: [Location!]!
     location(id: ID!): [Location]
   },
+
+  type Mutation{
+    # User Mutations
+    createUser( data: InputCreateUser!): User!
+    updateUser( id: ID!, data: InputUpdateUser!): User!
+    deleteUser(id: ID!): User!
+    deleteAllUSer: DeleteAllUser!
+
+    # Gereksiz uzayacağı için user dışındakileri yapmamaya karar verdim :/
+  }
 
 `;
 
@@ -79,6 +106,56 @@ const resolvers = {
     // Locations
     locations: () => locations,
     location: (parents, args ) => locations.filter( location => location.id == args.id )
+  },
+
+  Mutation: {
+    // User Mutations
+    createUser: (parent, {data:{username,email}}) => {
+      // Eğer daha önce girilen mail'e ait kullanıcı yoksa kullancıyı oluşturacak var ise hata mesajı yollayacak
+      const user = users.find(user => user.email == email)
+      if(!user){
+        const newUser ={
+          id: nanoid(),
+          email,
+          username
+        }
+        users.push(newUser)
+        return newUser
+      }
+      throw new Error('Kulllanıcı zaten kayıtlı')
+    },
+    updateUser: (parent, {id, data}) => {
+      // Eğer grilen id'de bir kullancı varsa id hariç diğer bilgileri argümanlardan alır.
+      const user_index = users.findIndex(user => user.id == id)
+      if(user_index === -1) throw new Error('User not found')
+      const updated_user = ( users[user_index] = {
+        ...users[user_index],
+        ...data,
+      });
+      return updated_user
+    },
+    deleteUser: (parent, {id}) =>{
+      // Eğer girilne id'de değer mevcut ise o değerin içindeki verileri boşaltır.
+      const user_index = users.findIndex(user => user.id == id)
+      if(user_index === -1) throw new Error('User not found')
+      const deletedUser = users[user_index]
+      users.splice(user_index, 1)
+      return deletedUser;
+    },
+    deleteAllUSer: () => {
+      // kullanıcılar dizinini sıfırdan başlayarak dizinin uzunluğuna göre boşaltır. Silinene toplam kullancıyı sayısını ve sililen kullancıcıları ekrana döndürür 
+      const count = users.length;
+      const deleted_users = []
+      deleted_users.push(...users);  
+
+      users.splice(0, users.length)
+      return {
+        deleted_users,
+        count
+      }
+    },
+
+
   },
 
   // Bu kısımda özel cevaplar oluşturdum
