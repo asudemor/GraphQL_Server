f9e6b2d9a52749110d9b0cd817c7d2821b45983c
nuhgnc <Nuh16358@gmail.com>
2022-05-26 16:02:51 +0300

    Folder/Schema Structure - 2 (File Loader)

--- src/graphql/resolvers/index.js
+++ src/graphql/resolvers/index.js
@@ -1,21 +1,4 @@
-/* const Comments = require ('./Comments')
-const Mutation = require ('./Mutation')
-const Post = require ('./Post')
-const Query = require ('./Query')
-const Subscription = require ('./Subscription')
-const User = require ('./User')
 
-
-module.exports = {
-    Comments,
-    Mutation,
-    Post,
-    Query,
-    Subscription,
-    User
-} */
-
-// ./graphql/resolvers.js
 const path = require('path');
 const { mergeResolvers } = require('@graphql-tools/merge');
 const { loadFilesSync } = require('@graphql-tools/load-files');
