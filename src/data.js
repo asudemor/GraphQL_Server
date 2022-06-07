748ae4f6eb79c82e7633253b4d94ef2c404bf695
nuhgnc <Nuh16358@gmail.com>
2022-06-07 19:50:07 +0300

    Post Detaylarının Gösterilmesi

--- src/data.js
+++ src/data.js
@@ -17,20 +17,23 @@ const posts = [
   {
     id: '1',
     title: 'Return, The',
-    description: '1 nolu postun açıklaması bu şeklide olacak',
+    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi maxime ab qui ullam pariatur. Nam a dolore doloremque dolor nemo, iure quod pariatur ea aperiam quos quo placeat! Corporis, inventore! Id error cumque nostrum natus culpa voluptates esse neque ex eligendi corrupti nam dolor, velit nesciunt amet cum accusantium et, tempora ut minima earum voluptatem sapiente nulla aut maiores. Minus? Amet a quia eveniet sit dolore laudantium obcaecati dignissimos velit, qui provident porro tempore sed maiores autem recusandae facere rerum aspernatur! Vitae amet iure accusantium nam consequatur quos commodi magnam!',
     user_id: '1',
+    cover: 'https://i.picsum.photos/id/1004/1280/720.jpg?hmac=nUecTZQlTl9WifsWVR_dCF7yUEx5Jz3jgYpYjbCXh2E'
   },
   {
     id: '2',
     title: 'Bullet for Joey, A',
-    description: '2 nolu postun açıklaması bu şeklide olacak',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. A corporis incidunt temporibus explicabo officia laudantium. Ad est totam modi nulla perferendis, ab facilis, itaque error aspernatur incidunt enim aliquid sit.',
     user_id: '1',
+    cover: 'https://i.picsum.photos/id/721/1280/720.jpg?hmac=PoPYPJ147dbwVMxxnctPuO9PtHkTVsQf_r9_3jFvy3o'
   },
   {
     id: '3',
     title: 'Ten Inch Hero',
-    description: '3 nolu postun açıklaması bu şeklide olacak',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur voluptates, deleniti fuga in, odit libero quisquam architecto dolore, ab magni eos eum. Distinctio veniam amet illo maxime magnam? Praesentium, ex?' ,
     user_id: '2',
+    cover: 'https://i.picsum.photos/id/269/1280/720.jpg?hmac=J1KYh3jJVuy38lvr9SH19x_FDx5roRu23YChcRd134Y'
   },
 ];
 
