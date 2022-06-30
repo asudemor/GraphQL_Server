2ae980ff69411919d16a0415eb5d83a4dd9b6059
nuh genc <66550954+nuhgnc@users.noreply.github.com>
2022-06-30 23:24:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,59 +1,78 @@
 const { ApolloServer, gql } = require('apollo-server');
-const { ApolloServerPluginLandingPageGraphQLPlayground} = require('apollo-server-core');
-const { authors, books} = require ('./data.js')
+const { ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-server-core');
 
+const { languages, continents, countries } = require('./data');
 
 const typeDefs = gql`
-  type Author {
-    id: ID!
-    name: String!
-    surname: String!
-    age: Int
-    books: [Book!]
-  }
-
-  type Book {
-    id: ID!
-    title: String!
-    author: Author
-    author_id: String!
-    score: Float
-    isPublished: Boolean!
-  }
-
-  type Query {
-    books: [Book!]
-    book(id: ID!) : Book! 
-
-    authors: [Author!]
-    author(id: ID!) : Author!
-  }
+    type Country {
+        id: ID!
+        name: String!
+        code: String!
+        language: Language
+        continent: Continent
+        continent_code: String
+    }
+    type Language {
+        id: ID!
+        name: String!
+        code: String!
+        country: Country
+        continent: Continent
+        continent_code: String
+    }
+    type Continent {
+        id: ID!
+        name: String!
+        code: String!
+        countries: [Country]
+    }
+    type Query {
+        countries: [Country!]
+        country(code: String!): Country!
+        languages: [Language!]
+        language(code: String!): Language!
+        continents: [Continent!]
+        continent(code: String!): Continent!
+    }
 `;
 
 const resolvers = {
-  Query: {
-    books: () => books,
-    book: (parent, args) => books.find( book => book.id === args.id),
+    Query: {
+        countries: () => countries,
+        country: (parent, args) => countries.find(country => country.code === args.code),
 
-    authors : () => authors,
-    author: (parent, args) => authors.find( author => author.id === args.id),
-  },
+        languages: () => languages,
+        language: (parent, args) => languages.find(language => language.code === args.code),
 
-  Book: {
-    author: (parent) => authors.find(author => author.id == parent.author_id),
-  },
-  
-  Author: {
-    books: (parent) =>  books.filter (book =>  book.author_id === parent.id),
-  }
+        continents: () => continents,
+        continent: (parent, args) => continents.find(continent => continent.code === args.code),
+    },
+
+    Country: {
+        language: (parent) => languages.find((language) => language.code === parent.code),
+        continent: (parent) => continents.find((continent) => continent.code === parent.continent_code),
+    },
+
+    Language: {
+        country: (parent) => countries.find((country) => country.code === parent.code),
+        continent: (parent) => continents.find((continent) => continent.code === parent.continent_code),
+    },
+
+    Continent: {
+        countries: (parent) => countries.filter((country) => country.continent_code === parent.code),
+    },
 };
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+const server = new ApolloServer({ 
+    typeDefs, 
+    resolvers, 
+    plugins: [
+    ApolloServerPluginLandingPageGraphQLPlayground({
+      // options
+    })
+  ]
 });
 
 server.listen().then(({ url }) => {
-  console.log('apollo server başaladı' + url);
+    console.log(`Apollo server is up at ${url}`);
 });
